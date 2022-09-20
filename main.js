81031cf93623238eb5736e2618ac0173e1e10221
Polgár Kristóf <polgarkristof10@gmail.com>
2022-09-20 13:21:25 +0200

    javitas1

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function szamit(){
     let suly = document.getElementById("sulybox").value;
     let magassag = document.getElementById("magassagbox").value;
 
-    let eredmeny = suly / (magassag/100) ** 2;
+    let eredmeny = (suly / (magassag/100) ** 2).toFixed(2);
     document.getElementById('eredmenybox').value = eredmeny;
 
 }
